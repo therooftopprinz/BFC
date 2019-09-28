407bcd6287f7acf91e82bc10fe27582a54428e4e
therooftopprinz <mynameisrainer@gmail.com>
2019-09-28 21:42:16 +0800

    command manager

---
+++ src/CommandManager.hpp
@@ -0,0 +1,97 @@
+#ifndef __CLIT_HPP__
+#define __CLIT_HPP__
+
+#include <map>
+#include <regex>
+#include <optional>
+#include <iostream>
+#include <string_view>
+#include <FixedFunctionObject.hpp>
+
+namespace bfc
+{
+
+class ArgsMap : public std::map<std::string, std::string>
+{
+public:
+    ArgsMap() = default;
+    template<typename T>
+    std::optional<T> argAs(const std::string_view& pKey) const
+    {
+        auto findit = find(pKey.data());
+        if (findit == end())
+        {
+            return std::nullopt;
+        }
+        T rv;
+        std::istringstream iss(findit->second);
+        iss >> rv;
+        return rv;
+    }
+    std::optional<std::string> arg(const std::string_view& pKey)
+    {
+        auto findit = find(pKey.data());
+        if (findit == end())
+        {
+            return std::nullopt;
+        }
+        return findit->second;
+    }
+};
+
+class CommandManager
+{
+public:
+    using CmdFnCb = LightFunctionObject<std::string(ArgsMap&&)>;
+    void addCommand(const std::string_view& pCmd, const CmdFnCb& pCallback)
+    {
+        mCmds.emplace(pCmd, pCallback);
+    }
+    void removeCommand(const std::string_view& pCmd)
+    {
+        mCmds.erase(pCmd.data());
+    }
+    std::string executeCommand(const std::string_view& pCmd)
+    {
+        using namespace std::string_literals;
+
+        const std::regex cmdArgsPattern("^([\\S]+)[\\s]+(.*)$");
+        std::string cmd;
+        std::string args;
+        ArgsMap argsMap;
+
+        std::cmatch cmdArgsPatternMatch;
+        if (std::regex_match(pCmd.data(), cmdArgsPatternMatch, cmdArgsPattern))
+        {
+            cmd = cmdArgsPatternMatch[1].str();
+            if (3==cmdArgsPatternMatch.size())
+            {
+                args = cmdArgsPatternMatch[2].str();
+            }
+        }
+
+        const std::regex argsPattern("([\\S]+)[\\s]*=[\\s]*([\\S]+)");
+        std::smatch argsPatternMatch; 
+        while (std::regex_search(args, argsPatternMatch, argsPattern))
+        {
+            if (3!=argsPatternMatch.size())
+            {
+                return "invalid argument:"s + argsPatternMatch[0].str();
+            }
+            argsMap.emplace(argsPatternMatch[1], argsPatternMatch[2]);
+            args = argsPatternMatch.suffix();
+        }
+        auto cmdfnIt = mCmds.find(cmd);
+        if (mCmds.end() == cmdfnIt)
+        {
+            return "command not found: "s+cmd;
+        }
+        return cmdfnIt->second(std::move(argsMap));
+    }
+private:
+    std::unordered_map<std::string, CmdFnCb> mCmds;
+};
+
+} // namespace bfc
+
+#endif // __CLIT_HPP__
---
+++ tests/CommandManagerTest.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+#include <CommandManager.hpp>
+
+using namespace bfc;
+
+TEST(CommandManager, ShouldExecute)
+{
+    CommandManager cmdMan;
+    cmdMan.executeCommand("window fd =1 trigger = manual size= 40");
+}
+
+TEST(CommandManager, ShouldFillArgs)
+{
+    CommandManager cmdMan;
+    int fd;
+    std::string trigger;
+    int size;
+    CommandManager::CmdFnCb fn = [&](ArgsMap&& pArgs) -> std::string
+    {
+        fd = *pArgs.argAs<int>("fd");
+        trigger = *pArgs.argAs<std::string>("trigger");
+        size = *pArgs.argAs<int>("size");
+        return "OK";
+    };
+    cmdMan.addCommand("window", fn);
+    ASSERT_EQ("OK", cmdMan.executeCommand("window fd =1 trigger = manual size= 40"));
+    EXPECT_EQ(1, fd);
+    EXPECT_EQ("manual", trigger);
+    EXPECT_EQ(40, size);
+}
