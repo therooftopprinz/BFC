b240089a7766d8b31fed76ff71831c7eb09bf27f
therooftopprinz <mynameisrainer@gmail.com>
2019-10-05 22:37:16 +0800

    FixedFunctionObject should copy

--- src/FixedFunctionObject.hpp
+++ src/FixedFunctionObject.hpp
@@ -2,7 +2,20 @@
 #define __LIGHTFN_HPP__
 
 #include <cstddef>
+#include <cstring>
 #include <functional>
+
+#include <iostream>
+template <typename>
+void ptype()
+{
+    std::cout << __PRETTY_FUNCTION__ << "\n";
+}
+template <typename T, T v>
+void pval()
+{
+    std::cout << __PRETTY_FUNCTION__ << "\n";
+}
 
 namespace bfc
 {
@@ -16,26 +29,35 @@ FixedFunctionObject() = default;
 FixedFunctionObject(const FixedFunctionObject& pOther)
 {
     pOther.mCopier(mObject, pOther.mObject);
-    mFn = pOther.mFn;
-    mDestroyer = pOther.mDestroyer;
-    mCopier = pOther.mCopier;
+    set(pOther);
 }
 
 FixedFunctionObject(FixedFunctionObject&& pOther)
 {
     std::memcpy(mObject, pOther.mObject, N);
-    mFn = pOther.mFn;
-    mDestroyer = pOther.mDestroyer;
-    mCopier = pOther.mCopier;
-    pOther.mFn = nullptr;
+    set(pOther);
     pOther.mDestroyer = nullptr;
-    pOther.mCopier = nullptr;
 }
 
-template <typename CallableObj>
+template <typename CallableObj, std::enable_if_t<!std::is_same_v<std::remove_reference_t<CallableObj>, FixedFunctionObject>>* p = nullptr>
 FixedFunctionObject(CallableObj&& pObj)
 {
     set(std::forward<CallableObj>(pObj));
+}
+
+FixedFunctionObject& operator=(const FixedFunctionObject& pOther)
+{
+    reset();
+    pOther.mCopier(mObject, pOther.mObject);
+    set(pOther);
+}
+
+FixedFunctionObject& operator=(FixedFunctionObject&& pOther)
+{
+    reset();
+    std::memcpy(mObject, pOther.mObject, N);
+    set(pOther);
+    pOther.mDestroyer = nullptr;
 }
 
 template <typename CallableObj>
@@ -56,8 +78,10 @@ void reset()
     if (mDestroyer)
     {
         mDestroyer(mObject);
+        mDestroyer = nullptr;
     }
 }
+
 ReturnType operator()(Args&&... pArgs)
 {
     if (mFn)
@@ -71,7 +95,7 @@ ReturnType operator()(Args&&... pArgs)
 }
 
 private:
-    template <typename CallableObj>
+    template <typename CallableObj, std::enable_if_t<!std::is_same_v<std::remove_reference_t<CallableObj>, FixedFunctionObject>>* p = nullptr>
     void set(CallableObj&& pObj)
     {
         using CallableObjType = std::decay_t<CallableObj>;
@@ -92,6 +116,14 @@ private:
             return (*((CallableObjType*)pObj))(std::forward<Args>(pArgs)...);
         };
     }
+
+    void set(const FixedFunctionObject& pOther)
+    {
+        mFn = pOther.mFn;
+        mDestroyer = pOther.mDestroyer;
+        mCopier = pOther.mCopier;
+    }
+
     uint8_t mObject[N];
     ReturnType (*mFn)(void*, Args&&...) = nullptr;
     void (*mDestroyer)(void*) = nullptr;
--- tests/FixedFunctionObjectTest.cpp
+++ tests/FixedFunctionObjectTest.cpp
@@ -5,6 +5,37 @@ using namespace bfc;
 
 struct TestClass
 {
+    static int count;
+    static int copy;
+    static int called;
+    TestClass()
+    {
+        count++;
+    }
+    TestClass(const TestClass&)
+    {
+        copy++;
+    }
+    TestClass(TestClass&&)
+    {
+        count++;
+    }
+    ~TestClass()
+    {
+        count--;
+    }
+    static void reset()
+    {
+        count = 0;
+        copy = 0;
+        called = 0;
+    }
+
+    void operator()()
+    {
+        called++;
+    }
+
     int increment(int pV)
     {
         return pV+1;
@@ -15,25 +46,29 @@ struct TestClass
     }
 };
 
+int TestClass::count = 0;
+int TestClass::copy = 0;
+int TestClass::called = 0;
+
 int increment(int pV)
 {
     return pV+1;
 }
 
-TEST(FixedFunctionObject, ShoulCallMemberFunction)
+TEST(FixedFunctionObject, ShouldCallMemberFunction)
 {
     TestClass tc;
     LightFunctionObject<int(int)> fn([&tc](int i)->int {return tc.increment(i);});
     EXPECT_EQ(42, fn(41));
 }
 
-TEST(FixedFunctionObject, ShoulCallNonMemberFunction)
+TEST(FixedFunctionObject, ShouldCallNonMemberFunction)
 {
     LightFunctionObject<int(int)> fn(increment);
     EXPECT_EQ(42, fn(41));
 }
 
-TEST(FixedFunctionObject, ShoulCallMemberFunctionMixedArgs)
+TEST(FixedFunctionObject, ShouldCallMemberFunctionMixedArgs)
 {
     TestClass tc;
     LightFunctionObject<void(int&,int)> fn([&tc](int& a, int b)->void {return tc.add(a,b);});
@@ -42,7 +77,33 @@ TEST(FixedFunctionObject, ShoulCallMemberFunctionMixedArgs)
     EXPECT_EQ(42, a);
 }
 
-TEST(FixedFunctionObject, ShoulThrowUnset)
+TEST(FixedFunctionObject, ShouldCallCallable)
+{
+    TestClass::reset();
+    LightFunctionObject<void()> fn{TestClass()};
+    fn();
+    EXPECT_EQ(1, TestClass::called);
+}
+
+TEST(FixedFunctionObject, ShouldCallDestruct)
+{
+    TestClass::reset();
+    {
+        LightFunctionObject<void()> fn{TestClass()};
+        EXPECT_EQ(1, TestClass::count);
+    }
+    EXPECT_EQ(0, TestClass::count);
+}
+
+TEST(FixedFunctionObject, ShouldCopyConstruct)
+{
+    TestClass::reset();
+    LightFunctionObject<void()> fn{TestClass()};
+    LightFunctionObject<void()> fn2(fn);
+    EXPECT_EQ(1, TestClass::copy);
+}
+
+TEST(FixedFunctionObject, ShouldThrowUnset)
 {
     LightFunctionObject<void()> fn;
     EXPECT_THROW(fn(), std::bad_function_call);
