f2fa5818fd84a58a5ee5f68b6dbaf0b03f12bb9d
therooftopprinz <mynameisrainer@gmail.com>
2020-05-26 18:13:25 +0800

    Fix formating.

--- src/FixedFunctionObject.hpp
+++ src/FixedFunctionObject.hpp
@@ -12,132 +12,128 @@ template <std::size_t N, typename ReturnType, typename... Args>
 class FixedFunctionObject
 {
 public:
-FixedFunctionObject() = default;
+    FixedFunctionObject() = default;
 
-FixedFunctionObject(const FixedFunctionObject& pOther)
-{
-    if (pOther)
+    FixedFunctionObject(const FixedFunctionObject &pOther)
     {
-        pOther.mCopier(mObject, pOther.mObject);
+        if (pOther)
+        {
+            pOther.mCopier(mObject, pOther.mObject);
+        }
+
+        set(pOther);
     }
 
-    set(pOther);
-}
-
-FixedFunctionObject(FixedFunctionObject&& pOther)
-{
-    if (pOther)
+    FixedFunctionObject(FixedFunctionObject &&pOther)
     {
-        pOther.mMover(mObject, pOther.mObject);
+        if (pOther)
+        {
+            pOther.mMover(mObject, pOther.mObject);
+        }
+
+        set(pOther);
+        pOther.clear();
     }
 
-    set(pOther);
-    pOther.clear();
-}
-
-template <typename CallableObj, std::enable_if_t<!std::is_same_v<std::remove_reference_t<CallableObj>, FixedFunctionObject>>* p = nullptr>
-FixedFunctionObject(CallableObj&& pObj)
-{
-    set(std::forward<CallableObj>(pObj));
-}
-
-FixedFunctionObject& operator=(const FixedFunctionObject& pOther)
-{
-    reset();
-
-    if (pOther)
+    template <typename CallableObj, std::enable_if_t<!std::is_same_v<std::remove_reference_t<CallableObj>, FixedFunctionObject>> *p = nullptr>
+    FixedFunctionObject(CallableObj &&pObj)
     {
-        pOther.mCopier(mObject, pOther.mObject);
+        set(std::forward<CallableObj>(pObj));
     }
 
-    set(pOther);
-    return *this;
-}
-
-FixedFunctionObject& operator=(FixedFunctionObject&& pOther)
-{
-    reset();
-
-    if (pOther)
+    FixedFunctionObject &operator=(const FixedFunctionObject &pOther)
     {
-        pOther.mMover(mObject, pOther.mObject);
+        reset();
+
+        if (pOther)
+        {
+            pOther.mCopier(mObject, pOther.mObject);
+        }
+
+        set(pOther);
+        return *this;
     }
 
-    set(pOther);
-    pOther.clear();
-    return *this;
-}
-
-template <typename CallableObj>
-FixedFunctionObject& operator=(CallableObj&& pObj)
-{
-    reset();
-    set(std::forward<CallableObj>(pObj));
-    return *this;
-}
-
-~FixedFunctionObject()
-{
-    reset();
-}
-
-operator bool() const
-{
-    return mFn;
-}
-
-void reset()
-{
-    if (mFn)
+    FixedFunctionObject &operator=(FixedFunctionObject &&pOther)
     {
-        mDestroyer(mObject);
+        reset();
+
+        if (pOther)
+        {
+            pOther.mMover(mObject, pOther.mObject);
+        }
+
+        set(pOther);
+        pOther.clear();
+        return *this;
     }
 
-    clear();
-}
+    template <typename CallableObj>
+    FixedFunctionObject &operator=(CallableObj &&pObj)
+    {
+        reset();
+        set(std::forward<CallableObj>(pObj));
+        return *this;
+    }
 
-template <typename... T>
-ReturnType operator()(T&&... pArgs) const
-{
-    if (mFn)
+    ~FixedFunctionObject()
     {
-        return mFn(mObject, std::forward<T>(pArgs)...);
+        reset();
     }
-    else
+
+    operator bool() const
     {
-        throw std::bad_function_call();
+        return mFn;
     }
-}
+
+    void reset()
+    {
+        if (mFn)
+        {
+            mDestroyer(mObject);
+        }
+
+        clear();
+    }
+
+    template <typename... T>
+    ReturnType operator()(T &&... pArgs) const
+    {
+        if (mFn)
+        {
+            return mFn(mObject, std::forward<T>(pArgs)...);
+        }
+        else
+        {
+            throw std::bad_function_call();
+        }
+    }
 
 private:
-    template <typename CallableObj, std::enable_if_t<!std::is_same_v<std::remove_reference_t<CallableObj>, FixedFunctionObject>>* p = nullptr>
-    void set(CallableObj&& pObj)
+    template <typename CallableObj, std::enable_if_t<!std::is_same_v<std::remove_reference_t<CallableObj>, FixedFunctionObject>> *p = nullptr>
+    void set(CallableObj &&pObj)
     {
         using CallableObjType = std::decay_t<CallableObj>;
-        static_assert(N>=sizeof(CallableObjType));
+        static_assert(N >= sizeof(CallableObjType));
         new (mObject) CallableObjType(std::forward<CallableObjType>(pObj));
-        mDestroyer = [](void* pObj)
-        {
-            ((CallableObjType*)pObj)->~CallableObjType();
+        mDestroyer = [](void *pObj) {
+            ((CallableObjType *)pObj)->~CallableObjType();
         };
 
-        mCopier = [](void* pObj, const void* pOther)
-        {
-            new (pObj) CallableObjType(*(const CallableObjType*)pOther);
+        mCopier = [](void *pObj, const void *pOther) {
+            new (pObj) CallableObjType(*(const CallableObjType *)pOther);
         };
 
-        mMover = [](void* pObj, void* pOther)
-        {
-            new (pObj) CallableObjType(std::move(*(CallableObjType*)pOther));
+        mMover = [](void *pObj, void *pOther) {
+            new (pObj) CallableObjType(std::move(*(CallableObjType *)pOther));
         };
 
-        mFn = [](const void* pObj, Args... pArgs) -> ReturnType
-        {
-            return (*((CallableObjType*)pObj))(std::forward<Args>(pArgs)...);
+        mFn = [](const void *pObj, Args... pArgs) -> ReturnType {
+            return (*((CallableObjType *)pObj))(std::forward<Args>(pArgs)...);
         };
     }
 
-    void set(const FixedFunctionObject& pOther)
+    void set(const FixedFunctionObject &pOther)
     {
         mFn = pOther.mFn;
         mDestroyer = pOther.mDestroyer;
@@ -156,21 +152,26 @@ private:
     }
 
     uint8_t mObject[N];
-    ReturnType (*mFn)(const void*, Args...) = nullptr;
-    void (*mDestroyer)(void*) = nullptr;
-    void (*mCopier)(void*, const void*) = nullptr;
-    void (*mMover)(void*, void*) = nullptr;
+    ReturnType (*mFn)(const void *, Args...) = nullptr;
+    void (*mDestroyer)(void *) = nullptr;
+    void (*mCopier)(void *, const void *) = nullptr;
+    void (*mMover)(void *, void *) = nullptr;
 };
 
-template <std::size_t N, typename T> struct FixedFunctionObjectTypeHelper;
-template <std::size_t N, typename ReturnType, typename... ArgsType> struct FixedFunctionObjectTypeHelper<N, ReturnType(ArgsType...)>
+template <std::size_t N, typename T>
+struct FixedFunctionObjectTypeHelper;
+template <std::size_t N, typename ReturnType, typename... ArgsType>
+struct FixedFunctionObjectTypeHelper<N, ReturnType(ArgsType...)>
 {
     using type = FixedFunctionObject<N, ReturnType, ArgsType...>;
 };
 
-template <typename FnType> using UltraLightFn = typename FixedFunctionObjectTypeHelper<8, FnType>::type;
-template <typename FnType> using LightFn = typename FixedFunctionObjectTypeHelper<24, FnType>::type;
-template <typename FnType> using HeavyFn = typename FixedFunctionObjectTypeHelper<32, FnType>::type;
+template <typename FnType>
+using UltraLightFn = typename FixedFunctionObjectTypeHelper<8, FnType>::type;
+template <typename FnType>
+using LightFn = typename FixedFunctionObjectTypeHelper<24, FnType>::type;
+template <typename FnType>
+using HeavyFn = typename FixedFunctionObjectTypeHelper<32, FnType>::type;
 
 } // namespace bfc
 
