411114022941779b7b7c274d7d696b9333c5bb43
therooftopprinz <mynameisrainer@gmail.com>
2020-05-31 11:00:03 +0800

    Preinit thead pool and block when free is empty.

--- src/ThreadPool.hpp
+++ src/ThreadPool.hpp
@@ -20,6 +20,45 @@ class ThreadPool
 {
 public:
     using Functor = FunctorType;
+    ThreadPool(size_t pMaxSize = 4)
+        : mMaxSize(pMaxSize)
+    {
+        std::unique_lock<std::mutex> lgFreeList(mFreeListMutex);
+        for (auto i=0u; i<mMaxSize; i++)
+        {
+            mFreeList.emplace_back(i);
+            mPool.emplace_back(std::make_unique<ThreadEntry>());
+            auto& entry = *(mPool.back());
+            entry.functor = {};
+
+            entry.thread = std::thread([this, useIndex = i, &entry]()
+                {
+                    auto pred = [&]() {return entry.functor || !mIsRunning;};
+                    while(true)
+                    {
+                        std::unique_lock<std::mutex> lgEntry(entry.entryMutex);
+
+                        if (entry.functor)
+                        {
+                            entry.functor();
+                            entry.functor = {};
+                            std::unique_lock<std::mutex> lgFreeList(mFreeListMutex);
+                            mFreeList.emplace_back(useIndex);
+                        }
+
+                        if (!mIsRunning)
+                        {
+                            return;
+                        }
+
+                        mMaxSizeCv.notify_one();
+
+                        entry.threadCv.wait(lgEntry, pred);
+                    }
+                });
+        }
+    }
+    
     ~ThreadPool()
     {
         mIsRunning = false;
@@ -31,65 +70,30 @@ public:
     }
     void execute(Functor pFunctor)
     {
-        std::size_t useIndex; 
+        std::unique_lock<std::mutex> lgFreeList(mFreeListMutex);
 
-        std::unique_lock<std::mutex> lg(mFreeListMutex);
-        if (mFreeList.size())
-        {
-            useIndex = mFreeList.back();
-            mFreeList.pop_back();
-            std::unique_lock<std::mutex> lgPool(mPoolMutex);
-            auto& entry = *mPool[useIndex];
-
-            std::unique_lock<std::mutex> lg(entry.entryMutex);
-            entry.functor = std::move(pFunctor);
-            entry.threadCv.notify_one();
-        }
-        else
-        {
-            std::unique_lock<std::mutex> lg(mPoolMutex);
-            mPool.emplace_back(std::make_unique<ThreadEntry>());
-            auto& entry = *(mPool.back());
-            useIndex = mPool.size()-1;
-
-            // TODO: not working with a std::move calls operator=(FixedFunctionObject&)
-            entry.functor = (Functor&&)pFunctor;
-
-            mPool[useIndex]->thread = std::thread([this, useIndex, &entry]()
-            {
-                auto pred = [&]() {return entry.functor || !mIsRunning;};
-                while(true)
-                {
-                    std::unique_lock<std::mutex> lg(entry.entryMutex);
-
-                    if (entry.functor)
-                    {
-                        entry.functor();
-                        entry.functor = {};
-                        std::unique_lock<std::mutex> lg(mFreeListMutex);
-                        mFreeList.emplace_back(useIndex);
-                    }
-
-                    if (!mIsRunning)
-                    {
-                        return;
-                    }
-
-                    entry.threadCv.wait(lg, pred);
-                }
+        mMaxSizeCv.wait(lgFreeList, [this](){
+                return mFreeList.size();
             });
-        }
+
+        auto useIndex = mFreeList.back();
+        mFreeList.pop_back();
+        auto& entry = *mPool[useIndex];
+
+        std::unique_lock<std::mutex> lgEntry(entry.entryMutex);
+        entry.functor = std::move(pFunctor);
+        entry.threadCv.notify_one();
     }
 
     std::size_t countActive() const
     {
-        std::unique_lock<std::mutex> lg(mFreeListMutex);
+        std::unique_lock<std::mutex> lgFreeList(mFreeListMutex);
         return mPool.size() - mFreeList.size();
     }
 
     std::size_t size() const
     {
-        std::unique_lock<std::mutex> lg(mPoolMutex);
+        std::unique_lock<std::mutex> lgFreeList(mFreeListMutex);
         return mPool.size();
     }
 private:
@@ -103,7 +107,8 @@ private:
 
     bool mIsRunning = true;
     std::vector<std::unique_ptr<ThreadEntry>> mPool;
-    mutable std::mutex mPoolMutex;
+    size_t mMaxSize = 8;
+    std::condition_variable mMaxSizeCv;
     std::vector<std::size_t> mFreeList;
     mutable std::mutex mFreeListMutex;
 };
